79de7bd5e654a69e718d382336550dd83feafc8c
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-12-21 14:48:58 -0700

    `Refactor SkillText component: moved motion.div inside a new div`

--- app/components/sub/SkillText.tsx
+++ app/components/sub/SkillText.tsx
@@ -12,17 +12,20 @@ import { SparklesIcon } from "@heroicons/react/16/solid";
 const SkillText = () => {
   return (
     <div className="w-full h-auto flex flex-col item-center justify-center">
-      <motion.div
-        variants={slideInFromTop} // Animation variant for sliding in from the top
-        className="welcome-box py-2 px-7 border border-[#7042f88b] rounded-full max-w-max opacity-[0.9] mt-[-20px]"
-      >
-        <div className="flex items-center">
-          <SparklesIcon className="text-[#b49bff] mr-2 h-5 w-5" />
-          <h1 className="welcome-text text-[13px] text-white">
-            Think Better with Next.Js 13
-          </h1>
-        </div>
-      </motion.div>
+      <div className="flex items-center justify-center">
+        <motion.div
+          variants={slideInFromTop} // Animation variant for sliding in from the top
+          className="welcome-box py-2 px-7 border border-[#7042f88b] rounded-full max-w-max opacity-[0.9] mt-[-20px]"
+        >
+          <div className="flex items-center">
+            <SparklesIcon className="text-[#b49bff] mr-2 h-5 w-5" />
+            <h1 className="welcome-text text-[13px] text-white">
+              Think Better with Next.Js 13
+            </h1>
+          </div>
+        </motion.div>
+      </div>
+
       <motion.div
         variants={slideInFromLeft(0.5)} // Animation variant for sliding in from the Left
         className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
@@ -31,7 +34,7 @@ const SkillText = () => {
       </motion.div>
       <motion.div
         variants={slideInFromRight(0.5)}
-        className="cursiv text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
+        className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
       >
         Never miss a task deadline
       </motion.div>
